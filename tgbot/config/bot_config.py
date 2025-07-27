83ab00f5bb76d693d04c37b9f9bfce9e2c96f5b4
RobunGid <robungidorig@gmail.com>
2025-07-27 23:21:52 +0300

    add default HTML parse mode

--- tgbot/config/bot_config.py
+++ tgbot/config/bot_config.py
@@ -5,6 +5,8 @@ from dotenv import dotenv_values
 from aiogram import Dispatcher
 from aiogram import Bot
 from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.enums.parse_mode import ParseMode
+from aiogram.client.default import DefaultBotProperties
 
 logging.basicConfig(level=logging.INFO)
 
@@ -16,6 +18,8 @@ if config["TOKEN"] is None:
 	raise ValueError("Bot token is required in /tgbot/.env")
 
 TOKEN = config["TOKEN"]
+
+DefaultBotProperties(parse_mode=ParseMode.HTML)
 
 bot = Bot(TOKEN)
 storage = MemoryStorage()
